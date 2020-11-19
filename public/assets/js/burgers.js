cdd26d91325a1c002777fcb82b1b602e84005b15
Wes <wmcmillan1012@gmail.com>
2020-11-19 14:19:33 -0800

    chenged some syntax

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,20 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-eaten").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    var newEaten = $(this).data("neweaten");
 
-    var newSleepState = {
-      sleepy: newSleep
+    var newEatenState = {
+      Eaten: newEat
     };
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newEatState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed eaten to", newEat);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -27,7 +27,7 @@ $(function() {
 
     var newBurger = {
       name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+      sleepy: $("[name=eaten]:checked").val().trim()
     };
 
     // Send the POST request.
