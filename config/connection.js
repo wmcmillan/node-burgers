5a6b8bc9fa9081c7f877f05a11181821c3610a75
Wes <wmcmillan1012@gmail.com>
2020-11-19 12:54:11 -0800

    changed some syntax and added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
-  database: "burger
-  
-  _db"
+  database: "burger_db",
 });
 
 // Make connection.
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Dicks Burger');
+INSERT INTO burgers (name) VALUES ('Shake Shack Burger');
+INSERT INTO burgers (name, eaten) VALUES ('In N Out Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('What a Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('8oz Burger', true);
+INSERT INTO burgers (name) VALUES ('Fat Burger');
